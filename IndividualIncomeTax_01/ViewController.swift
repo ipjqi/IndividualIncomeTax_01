519d94b72a71ffff4b210ccb8384748802741d43
潘镜全 <pjq@pjq.local>
2015-06-24 22:36:09 +0800

    Initial Commit

---
+++ IndividualIncomeTax_01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IndividualIncomeTax_01
+//
+//  Created by 潘镜全 on 15/6/24.
+//  Copyright (c) 2015年 ipjqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
